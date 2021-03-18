b331eef6033ea179118baa8dc50d90e101c19a2a
waraich1 <waraich@mcmaster.ca>
2021-03-17 22:16:49 -0400

    Setup the folder structure

---
+++ frontend/src/components/asyncComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+ 
+const reduxComponent  = () => (
+  <h1>Async calls also works</h1>
+);
+ 
+export default reduxComponent;
---
+++ frontend/src/components/simpleComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+ 
+const simpleComponent  = () => (
+  <h1>This also Works</h1>
+);
+ 
+export default simpleComponent;
